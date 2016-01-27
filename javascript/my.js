e6ab51ea610dea5c22e42847fb3c6f910ab21609
snavien <smvien@ucdavis.edu>
2016-01-27 15:05:45 -0800

    fixed counts

--- javascript/my.js
+++ javascript/my.js
@@ -5,7 +5,7 @@
 
 
 
-
+/** WINDOW DIMENSIONS **/
 // set dimensions of the canvas / graph
 var base_width = $(window).width();
 var base_height = $(window).height();
@@ -16,16 +16,18 @@ var margin = {top: 20, right: 20, bottom: 60, left: 250},
 				  height = base_height - margin.top - margin.bottom - 100;
 
 
+/** OVERVIEW CHART**/
 //TODO: Create overview of actions or servers
-
-
 var width = 960,
     height = 500,
     radius = Math.min(width, height) / 2;
 
 var min_cnt = 0,
 		max_cnt = 0;
-var color = d3.scale.linear.do-main([min_cnt, max_cnt]).range("red", "black");
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+//var color = d3.scale.linear.do-main([min_cnt, max_cnt]).range("red", "black");
 
 var arc = d3.svg.arc()
     .outerRadius(radius - 10)
@@ -35,28 +37,30 @@ var pie = d3.layout.pie()
     .sort(null)
     .value(function(d) { return d.count; });
 
-var svg = d3.select("body").append("svg")
+
+var svg = d3.select("#donutchart").append("svg")
     .attr("width", width)
     .attr("height", height)
-  .append("g")
+  	.append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 d3.csv("all_actions.csv", type, function(error, data) {
 	  if (error) throw error;
 
+
 	  var g = svg.selectAll(".arc")
 	      .data(pie(data))
-	    .enter().append("g")
+	    	.enter().append("g")
 	      .attr("class", "arc");
 
 	  g.append("path")
 	      .attr("d", arc)
-	      .style("fill", function(d) { return color(d.data.age); });
+	      .style("fill", function(d) { return color(d.data.count); });
 
 	  g.append("text")
 	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
 	      .attr("dy", ".35em")
-	      .text(function(d) { return d.data.age; });
+	      .text(function(d) { return d.data.actions; });
 	});
 
 	function type(d) {
@@ -66,221 +70,221 @@ d3.csv("all_actions.csv", type, function(error, data) {
 
 </script>
 
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, height], .1); //players
-var y = d3.scale.linear().range([width, 0]); //time
-
-// define the axes
-var xAxis = d3.svg.axis()
-					.ticks(20)
-					.scale(y)
-    			.orient("bottom");
-var yAxis = d3.svg.axis()
-					.ticks(5)
-			    .scale(x)
-			    .orient("left")
-			    .ticks(10);
-
-// add the svg canvas to the div with id = barchart
-var svg = d3.select("#barchart").append("svg")
-	    	.attr("width", width + margin.left + margin.right)
-	    	.attr("height", height + margin.top + margin.bottom)
-	    	.attr("class", "barchart_svg")
-	  		.append("g")
-	    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// this div is used for the tooltip
-var div = d3.select("body").append("div")
-		    .attr("class", "tooltip")
-		    .style("opacity", 0);
-
-// Load the data, process it, and display it with a bar chart.
-// You can't load the fullsize file, so you'll need to do some
-// preprocessing to break the data up or aggregate it
-d3.csv("data/all_actions.csv", function(error, data) {
-	if (error) throw error;
-	// var player_data = data.filter(function(d) {
-	// 	return d.player_b === "00040857941a98d183a9ffcc5efc12a5e73a91ad")
-	// });
-
-	//make a selection box
-	//create unique list of player b's
-	var player_b = [], count = 0;
-	var u = {};
-	for(var i = 0; i < data.length; i++){
-	  if(u.hasOwnProperty(data[i].player_b)) {
-	     continue;
-	  }
-	  player_b.push(data[i].player_b);
-		count++;
-	  u[data[i].player_b] = 1;
- 	}
-
-	console.log(player_b);
-	var player_data = [];
-
-	var select = d3.select('body')
-							.append('select')
-							.attr('class', 'select_pb')
-							.on('change', onchange(player_data));
-	var option = select
-							.selectAll('option')
-							.data(player_b).enter()
-							.append('option')
-							.text(function(d) {return d;});
-	function onchange(player_data){
-		selectValue = d3.select('select').property('value');
-		d3.select('body')
-			.append('p')
-			.text(selectValue + ' is the last selected option.');
-
-			return function set_pd(){
-
-			count = 0;
-			alert(player_data);
-			for(var i = 0; i < data.length; i++){
-				var pb = d3.select('select').property('value');
-				console.log(pb);
-				if(data[i].player_b == " "+ pb)
-				{
-					player_data[count] = data[i];
-					count++;
-				}
-			}
-		};
-	};
-
-
-
-
-
-
-	// get the total time spent on each key
-	var max = d3.max(player_data, function(d) {
-		return +d.stop_t/1000;
-//  	return d3.max(d.stop_t, function(e) { return d3.max(e); });
-	});
-
-	var min = d3.min(player_data, function(d) {
-		return +d.start_t/1000;
-		//return d3.min(d.start_t, function(e) { return d3.min(e); });
-	});
-	console.log(max);
-	console.log(min);
-//	var max = d3.max(data, function(d){return +d.stop_t};);
-		//var min = d3.min(player_data, function(d){return +d.start_t};)
-	var times = calc_time_per_key(player_data);
-
-//	var min = d3.min(player_data, function(d){return +d.start_t};)
-	// scale the data ranges
-	// the x domain goes over the set of keys
-	x.domain(player_data.map(function(d) { return d.player_a; }));
-	// y goes from 0 to the max value in times
-
-	y.domain([max,min]);
-
-	// add the axes
-	svg.append("g")
-	  	.attr("class", "x axis")
-			.attr("transform", "translate(0," + height + ")")
-	  	.call(xAxis)
-			.append("text")
-			.attr("x", width/2)
-			.attr("y", 30)
-			.style("text-anchor", "middle")
-			.text("Time (1000m)");
-			// .append("text")
-			// .attr("y", 450)
-			// .style("text-anchor", "end")
-			// .text("Time");
-
-	svg.append("g")
-	  .attr("class", "y axis")
-		.call(yAxis)
-		.append("text")
-		//.attr("transform", "rotate(180)")
-		.attr("y", -40)
-		.attr("dy", "3em")
-		.style("text-anchor", "end")
-		.text("Player A");
-
-	console.log(player_data[0].server_id);
-	console.log(player_data[0].start_t);
-
-	// add the dots
-	svg.selectAll(".dot")
-		.data(player_data)
-		.enter().append("circle")
-			.attr("class", "dot")
-      .attr("r", function(d){ return d.stop_t - d.start_t; })
-			.attr("fill", function(d){ if(d.key == " KilledBy") return "red"; else return "blue";})
-      .attr("cy", function(d) {
-				console.log(d.player_a);
-				return x(d.player_a); })
-			.attr("fill-opacity", 0.70)
-      .attr("cx", function(d) { return y(d.start_t/1000); })
-
-			//.attr("x", )
-			//.attr("width", x.rangeBand())
-			//.attr("y", )
-			//.attr("height", function(d) { return height - y(d.total_time); })
-			.on("mouseover", function(d) {
-				d3.select(this).attr("r", d3.select(this).attr("r") * 1 * 2);
-				if(d.key == " KilledBy"){
-					div.transition()
-						.duration(200)
-						.style("opacity", .95);
-					div.html("killed " + d.player_a + " for " + (d.stop_t - d.start_t) + "min")
-						.style("left", (x(d.key) + x.rangeBand() + x.rangeBand()/2) + "px")
-						.style("top", (d3.event.pageY ) + "px")
-						.style("font", "9.5px arial, serif")
-				}
-				else{
-					div.transition()
-						.duration(200)
-						.style("opacity", .95);
-					div.html("chatted with " + d.player_a + " for " + (d.stop_t - d.start_t) + "min")
-						.style("left", (x(d.key) + x.rangeBand() + x.rangeBand()/2) + "px")
-						.style("top", (d3.event.pageY - 28) + "px")
-						.style("font", "9.5px arial, serif")
-				}
-
-				})
-			.on("mouseout", function(d) {
-				d3.select(this).attr("r", (d.stop_t - d.start_t));
-				div.transition()
-                	.duration(500)
-                	.style("opacity", 0);
-				});
-	console.log("done");
-});
-
-// this gets the total time spent on each key
-// from on the loaded file and adds
-// it to a javascript object called time_per_key.
-function calc_time_per_key(data) {
-  var time_per_key = {};
-  for(var i = 0; i < data.length; i+=1)
-  {
-    var row = data[i];
-
-		if(row.key in time_per_key)
-    {
-			time_per_key[row.key] += row.stop_t - row.start_t;
-    }
-    else
-    {
-			time_per_key[row.key] = row.stop_t - row.start_t;
-    }
-  }
-
-  //convert to array form
-  var time_per_key_array = Object.keys(time_per_key).map(function (key) {
-    return {
-    	"key": key,
-      	"total_time": time_per_key[key]
-    };
-  });
-
-  return time_per_key_array;
-}
+// // set the ranges
+// var x = d3.scale.ordinal().rangeRoundBands([0, height], .1); //players
+// var y = d3.scale.linear().range([width, 0]); //time
+//
+// // define the axes
+// var xAxis = d3.svg.axis()
+// 					.ticks(20)
+// 					.scale(y)
+//     			.orient("bottom");
+// var yAxis = d3.svg.axis()
+// 					.ticks(5)
+// 			    .scale(x)
+// 			    .orient("left")
+// 			    .ticks(10);
+//
+// // add the svg canvas to the div with id = bubblechart
+// var svg = d3.select("#bubblechart").append("svg")
+// 	    	.attr("width", width + margin.left + margin.right)
+// 	    	.attr("height", height + margin.top + margin.bottom)
+// 	    	.attr("class", "barchart_svg")
+// 	  		.append("g")
+// 	    	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//
+// // this div is used for the tooltip
+// var div = d3.select("body").append("div")
+// 		    .attr("class", "tooltip")
+// 		    .style("opacity", 0);
+//
+// // Load the data, process it, and display it with a bar chart.
+// // You can't load the fullsize file, so you'll need to do some
+// // preprocessing to break the data up or aggregate it
+// d3.csv("data/all_actions.csv", function(error, data) {
+// 	if (error) throw error;
+// 	// var player_data = data.filter(function(d) {
+// 	// 	return d.player_b === "00040857941a98d183a9ffcc5efc12a5e73a91ad")
+// 	// });
+//
+// 	//make a selection box
+// 	//create unique list of player b's
+// 	var player_b = [], count = 0;
+// 	var u = {};
+// 	for(var i = 0; i < data.length; i++){
+// 	  if(u.hasOwnProperty(data[i].player_b)) {
+// 	     continue;
+// 	  }
+// 	  player_b.push(data[i].player_b);
+// 		count++;
+// 	  u[data[i].player_b] = 1;
+//  	}
+//
+// 	console.log(player_b);
+// 	var player_data = [];
+//
+// 	var select = d3.select('body')
+// 							.append('select')
+// 							.attr('class', 'select_pb')
+// 							.on('change', onchange(player_data));
+// 	var option = select
+// 							.selectAll('option')
+// 							.data(player_b).enter()
+// 							.append('option')
+// 							.text(function(d) {return d;});
+// 	function onchange(player_data){
+// 		selectValue = d3.select('select').property('value');
+// 		d3.select('body')
+// 			.append('p')
+// 			.text(selectValue + ' is the last selected option.');
+//
+// 			return function set_pd(){
+//
+// 			count = 0;
+// 			alert(player_data);
+// 			for(var i = 0; i < data.length; i++){
+// 				var pb = d3.select('select').property('value');
+// 				console.log(pb);
+// 				if(data[i].player_b == " "+ pb)
+// 				{
+// 					player_data[count] = data[i];
+// 					count++;
+// 				}
+// 			}
+// 		};
+// 	};
+//
+//
+//
+//
+//
+//
+// 	// get the total time spent on each key
+// 	var max = d3.max(player_data, function(d) {
+// 		return +d.stop_t/1000;
+// //  	return d3.max(d.stop_t, function(e) { return d3.max(e); });
+// 	});
+//
+// 	var min = d3.min(player_data, function(d) {
+// 		return +d.start_t/1000;
+// 		//return d3.min(d.start_t, function(e) { return d3.min(e); });
+// 	});
+// 	console.log(max);
+// 	console.log(min);
+// //	var max = d3.max(data, function(d){return +d.stop_t};);
+// 		//var min = d3.min(player_data, function(d){return +d.start_t};)
+// 	var times = calc_time_per_key(player_data);
+//
+// //	var min = d3.min(player_data, function(d){return +d.start_t};)
+// 	// scale the data ranges
+// 	// the x domain goes over the set of keys
+// 	x.domain(player_data.map(function(d) { return d.player_a; }));
+// 	// y goes from 0 to the max value in times
+//
+// 	y.domain([max,min]);
+//
+// 	// add the axes
+// 	svg.append("g")
+// 	  	.attr("class", "x axis")
+// 			.attr("transform", "translate(0," + height + ")")
+// 	  	.call(xAxis)
+// 			.append("text")
+// 			.attr("x", width/2)
+// 			.attr("y", 30)
+// 			.style("text-anchor", "middle")
+// 			.text("Time (1000m)");
+// 			// .append("text")
+// 			// .attr("y", 450)
+// 			// .style("text-anchor", "end")
+// 			// .text("Time");
+//
+// 	svg.append("g")
+// 	  .attr("class", "y axis")
+// 		.call(yAxis)
+// 		.append("text")
+// 		//.attr("transform", "rotate(180)")
+// 		.attr("y", -40)
+// 		.attr("dy", "3em")
+// 		.style("text-anchor", "end")
+// 		.text("Player A");
+//
+// 	console.log(player_data[0].server_id);
+// 	console.log(player_data[0].start_t);
+//
+// 	// add the dots
+// 	svg.selectAll(".dot")
+// 		.data(player_data)
+// 		.enter().append("circle")
+// 			.attr("class", "dot")
+//       .attr("r", function(d){ return d.stop_t - d.start_t; })
+// 			.attr("fill", function(d){ if(d.key == " KilledBy") return "red"; else return "blue";})
+//       .attr("cy", function(d) {
+// 				console.log(d.player_a);
+// 				return x(d.player_a); })
+// 			.attr("fill-opacity", 0.70)
+//       .attr("cx", function(d) { return y(d.start_t/1000); })
+//
+// 			//.attr("x", )
+// 			//.attr("width", x.rangeBand())
+// 			//.attr("y", )
+// 			//.attr("height", function(d) { return height - y(d.total_time); })
+// 			.on("mouseover", function(d) {
+// 				d3.select(this).attr("r", d3.select(this).attr("r") * 1 * 2);
+// 				if(d.key == " KilledBy"){
+// 					div.transition()
+// 						.duration(200)
+// 						.style("opacity", .95);
+// 					div.html("killed " + d.player_a + " for " + (d.stop_t - d.start_t) + "min")
+// 						.style("left", (x(d.key) + x.rangeBand() + x.rangeBand()/2) + "px")
+// 						.style("top", (d3.event.pageY ) + "px")
+// 						.style("font", "9.5px arial, serif")
+// 				}
+// 				else{
+// 					div.transition()
+// 						.duration(200)
+// 						.style("opacity", .95);
+// 					div.html("chatted with " + d.player_a + " for " + (d.stop_t - d.start_t) + "min")
+// 						.style("left", (x(d.key) + x.rangeBand() + x.rangeBand()/2) + "px")
+// 						.style("top", (d3.event.pageY - 28) + "px")
+// 						.style("font", "9.5px arial, serif")
+// 				}
+//
+// 				})
+// 			.on("mouseout", function(d) {
+// 				d3.select(this).attr("r", (d.stop_t - d.start_t));
+// 				div.transition()
+//                 	.duration(500)
+//                 	.style("opacity", 0);
+// 				});
+// 	console.log("done");
+// });
+//
+// // this gets the total time spent on each key
+// // from on the loaded file and adds
+// // it to a javascript object called time_per_key.
+// function calc_time_per_key(data) {
+//   var time_per_key = {};
+//   for(var i = 0; i < data.length; i+=1)
+//   {
+//     var row = data[i];
+//
+// 		if(row.key in time_per_key)
+//     {
+// 			time_per_key[row.key] += row.stop_t - row.start_t;
+//     }
+//     else
+//     {
+// 			time_per_key[row.key] = row.stop_t - row.start_t;
+//     }
+//   }
+//
+//   //convert to array form
+//   var time_per_key_array = Object.keys(time_per_key).map(function (key) {
+//     return {
+//     	"key": key,
+//       	"total_time": time_per_key[key]
+//     };
+//   });
+//
+//   return time_per_key_array;
+// }
